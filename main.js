c3412ba7c933b82761b4cf8f627d41b74713955d
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2021-09-13 16:52:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function take_picture(){
 }
 
 console.log("ml5:version:",ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/q6I4vXDp7/",modelloaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/q6I4vXDp7/model.json",modelloaded);
 
 function modelloaded(){
     console.log("modelloaded");
